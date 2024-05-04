6ae3ee6fea81cf1dfc218fac299914cd46f0db1f
asheshmagar <asheshmagar8888667@gmail.com>
2024-05-04 20:23:21 +0545

    feat: add author uri

--- infinite-moving-cards.php
+++ infinite-moving-cards.php
@@ -1,11 +1,12 @@
 <?php
 /**
  * Plugin Name:       Infinite Moving Cards
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       Infinite moving cards block.
  * Requires at least: 6.1
  * Requires PHP:      7.4
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            asheshmagar
+ * Author URI:        https://asheshthapa.com.np/
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       infinite-moving-cards
